2f8ad60abe169645f9027e7bf3188add97392f5b
MaryemBess <47980211+MaryemBess@users.noreply.github.com>
2022-03-14 18:47:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const app = require('./app');
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -8,91 +9,90 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB,{
-  useNewUrlParser: true,
-  useCreateIndex:true,
-  useFindAndModify: false
-})
-.then(()=>console.log('DB Connected Successfully'));
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connected Successfully'));
 
 const UserSchema = new mongoose.Schema({
-name: { 
-  type: String,
-  required: true
-},
-lastname: {
-  type: String,
-  required:true
-},
-login: {
-  type: String,
-  required:true
-},
-password: {
-  type: String,
-  required:true
-},
-email: {
-  type: String,
-  required:true
-},
-role: {
-  type: String,
-  required:true
-},
-sex: {
-  type: String,
-  required:true
-},
-address: {
-  type: String,
-  required:true
-},
-telephone: {
-  type: String,
-  required:true
-},
-dateofbirth: {
-  type: Date,
-  required:true
-},
-age: {
-  type: Number,
-  required:true
-},
+  name: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  login: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  sex: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  telephone: {
+    type: String,
+    required: true,
+  },
+  dateofbirth: {
+    type: Date,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
 });
 const PatientSchema = new mongoose.Schema({
-  insurance: { 
+  insurance: {
     type: String,
-    required: true
+    required: true,
   },
   payment_method: {
     type: String,
-    required:true
+    required: true,
   },
   blood_type: {
     type: String,
-    required:true
+    required: true,
   },
   addictions: {
     type: String,
-    required:true
+    required: true,
   },
   height: {
     type: String,
-    required:true
+    required: true,
   },
   weight: {
     type: String,
-    required:true
+    required: true,
   },
-  });
-const User = mongoose.model('User',UserSchema);
-const Patient = mongoose.model('User',PatientSchema);
-
-
-
-
+});
+// eslint-disable-next-line no-unused-vars
+const User = mongoose.model('User', UserSchema);
+// eslint-disable-next-line no-unused-vars
+const Patient = mongoose.model('User', PatientSchema);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
