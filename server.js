2cddb8e98d3e331569e446c789f6eaeb57937148
yesminebaoueb <yasmine.baouab@esprit.tn>
2022-03-14 19:02:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,64 +8,109 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB,{
-  useNewUrlParser: true,
-  useCreateIndex:true,
-  useFindAndModify: false
-})
-.then(()=>console.log('DB Connected Successfully'));
+mongoose.connect(DB, {})
+  .then(() => console.log('DB Connected Successfully'));
 
 const UserSchema = new mongoose.Schema({
-name: { 
-  type: String,
-  required: true
-},
-lastname: {
-  type: String,
-  required:true
-},
-login: {
-  type: String,
-  required:true
-},
-password: {
-  type: String,
-  required:true
-},
-email: {
-  type: String,
-  required:true
-},
-role: {
-  type: String,
-  required:true
-},
-sex: {
-  type: String,
-  required:true
-},
-address: {
-  type: String,
-  required:true
-},
-telephone: {
-  type: String,
-  required:true
-},
-dateofbirth: {
-  type: Date,
-  required:true
-},
-age: {
-  type: Number,
-  required:true
-},
+  name: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  login: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  sex: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  telephone: {
+    type: String,
+    required: true
+  },
+  dateofbirth: {
+    type: Date,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
 });
 
-const User = mongoose.model('User',UserSchema);
+const User = mongoose.model('User', UserSchema);
 
+//DOCTOR
+const DoctorSchema = new mongoose.Schema({
+  specialty: {
+    type: String,
+    required: true
+  },
+  office_address: {
+    type: String,
+    required: true
+  },
+  office_number: {
+    type: String,
+    required: true
+  },
+  doctorate: {
+    type: String,
+    required: true
+  },
+  consult_price: {
+    type: Number,
+    required: true
+  },
+  coords: {
+    type: Number,
+    required: true
+  },
+});
 
+const Doctor = mongoose.model('Doctor', DoctorSchema);
 
+//MedicalDocs
+const MedicalDocsSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  dateofrelease: {
+    type: Date,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+});
+
+const MedicalDocs = mongoose.model('MedicalDocs', MedicalDocsSchema);
 
 
 const port = process.env.PORT || 8000;
