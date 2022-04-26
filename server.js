0c3631be4f6770d80987b567a9b62c2ebfa24c06
MohamedAmineLadjimi <ladjimi.mohamedamine@esprit.tn>
2022-04-26 21:13:11 +0100

    role changes
    changed roles .

--- server.js
+++ server.js
@@ -17,22 +17,22 @@ function initial() {
   Role.estimatedDocumentCount((err, count) => {
     if (!err && count === 0) {
       new Role({
-        name: 'user',
+        name: 'patient',
         // eslint-disable-next-line no-shadow
       }).save((err) => {
         if (err) {
           console.log('error', err);
         }
-        console.log("added 'user' to roles collection");
+        console.log("added 'patient' to roles collection");
       });
       new Role({
-        name: 'moderator',
+        name: 'doctor',
         // eslint-disable-next-line no-unused-vars
       }).save((error) => {
         if (err) {
           console.log('error', err);
         }
-        console.log("added 'moderator' to roles collection");
+        console.log("added 'doctor' to roles collection");
       });
       new Role({
         name: 'admin',
